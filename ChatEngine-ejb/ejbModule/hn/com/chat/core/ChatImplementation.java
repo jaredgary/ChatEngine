a086626aff06dc60ab8f022bdfe5ae7c33807785
jared <jared@DESKTOP-OOIHOOE>
2017-05-01 14:17:15 -0600

    Creación de implementación.

---
+++ ChatEngine-ejb/ejbModule/hn/com/chat/core/ChatImplementation.java
@@ -0,0 +1,35 @@
+package hn.com.chat.core;
+
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+/**
+ * The class ChatImplementation contains the imnplementations of all operations
+ * for ChatEngine-ejb project.
+ *
+ * @author Gary Gonzalez <mailto:garygonzalezzepeda@gmail.com/>
+ * @version 1.0.0
+ * @since May 1, 2017
+ */
+@Stateless
+@WebService(serviceName = "ChatComponent", endpointInterface = "hn.com.chat.core.IChatOperations")
+public class ChatImplementation implements IChatOperations {
+
+	/**
+	 * Instantiates a new chat implementation.
+	 */
+	public ChatImplementation() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see hn.com.chat.core.IChatOperations#sendTxtMsg(java.lang.String,
+	 * java.lang.String, java.lang.String)
+	 */
+	@Override
+	public String sendTxtMsg(final String to, final String from, final String msg) {
+		return null;
+	}
+
+}
