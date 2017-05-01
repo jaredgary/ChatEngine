712c65fd500aeabf2949230880a53943e74ce3d7
jared <jared@DESKTOP-OOIHOOE>
2017-05-01 14:13:29 -0600

    Interface de operaciones

---
+++ ChatEngine-ejb/ejbModule/hn/com/chat/core/IChatOperations.java
@@ -0,0 +1,36 @@
+package hn.com.chat.core;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+
+/**
+ * The interface IChatOperations contains the methods operations for
+ * ChatEngine-ejb project.
+ *
+ * @author Gary Gonzalez <mailto:garygonzalezzepeda@gmail.com/>
+ * @version 1.0.0
+ * @since May 1, 2017
+ */
+@WebService(name = "ChatComponent", targetNamespace = "http://chat.com/ChatComponent/v1")
+@SOAPBinding(parameterStyle = ParameterStyle.WRAPPED)
+public interface IChatOperations {
+
+	/**
+	 * Send txt msg.
+	 *
+	 * @param to
+	 *            the to
+	 * @param from
+	 *            the from
+	 * @param msg
+	 *            the msg
+	 * @return the string
+	 */
+	@WebMethod(operationName = "sendTxtMsg")
+	String sendTxtMsg(@WebParam(name = "to") String to, @WebParam(name = "from") String from,
+			@WebParam(name = "msg") String msg);
+
+}
